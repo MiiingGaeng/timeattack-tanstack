eb621adfd87131dc0b2e559b4f641aa0dfa16691
miing_gaeng <pem4134@naver.com>
2025-03-07 12:41:15 +0900

    refactor: 좋아요 기능 tanstack으로 리팩토링

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,45 +1,43 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { todoApi } from "../api/todos";
+import { QUERY_KEY } from "../constants/queryKey";
+import { useTodosQuery } from "../hooks/useTodosQuery";
 
 export default function TodoList() {
   const navigate = useNavigate();
   // TODO: 선택: useQuery 를 useTodosQuery 커스텀훅으로 정리해 보세요.
-  const {
-    data: todos,
-    error,
-    isPending,
-    refetch,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: async () => {
-      const response = await todoApi.get("/todos");
-      return response.data;
-    },
-  });
+  const { data: todos, error, isPending } = useTodosQuery();
 
   // TODO: 필수: 아래 handleLike 로 구현되어 있는 부분을 useMutation 으로 리팩터링 해보세요. 모든 기능은 동일하게 동작해야 합니다.
   // TODO: 선택: useMutation 으로 리팩터링 후, useTodoMutation 커스텀훅으로 정리해 보세요.
   const queryClient = useQueryClient();
-  const handleLike = async (id, currentLiked) => {
-    const previousTodos = [...todos];
-    try {
-      queryClient.setQueryData(["todos"], (prev) =>
-        prev.map((todo) =>
-          todo.id === id ? { ...todo, liked: !todo.liked } : todo,
-        ),
-      );
-      await todoApi.patch(`/todos/${id}`, {
-        liked: !currentLiked,
-      });
-    } catch (err) {
-      console.error(err);
-      queryClient.setQueryData(["todos"], previousTodos);
-    } finally {
-      refetch();
-    }
+
+  const updateTodoLike = async ({ id, liked }) => {
+    await todoApi.patch(`/todos/${id}`, { liked: !liked });
   };
+
+  const { mutate: likeMutation } = useMutation({
+    mutationFn: updateTodoLike,
+    onMutate: async ({ id, liked }) => {
+      await queryClient.cancelQueries([QUERY_KEY.TODOS, id]);
+      const prevTodo = queryClient.getQueryData([QUERY_KEY.TODOS, id]);
+
+      const updatedTodo = { ...prevTodo, liked: !liked };
+
+      queryClient.setQueryData([QUERY_KEY.TODOS, id], updatedTodo);
+
+      return { prevTodo };
+    },
+    onError: (error, _, context) => {
+      console.error("liked error : ", error);
+      queryClient.setQueryData(context.prevTodo);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries([QUERY_KEY.TODOS]);
+    }
+  });
 
   if (isPending) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
@@ -60,7 +58,7 @@ export default function TodoList() {
           style={{
             border: "1px solid black",
             padding: "10px",
-            marginBottom: "10px",
+            marginBottom: "10px"
           }}
         >
           <h3>{todo.title}</h3>
@@ -70,12 +68,12 @@ export default function TodoList() {
             </button>
             {todo.liked ? (
               <FaHeart
-                onClick={() => handleLike(todo.id, todo.liked)}
+                onClick={() => likeMutation({ id: todo.id, liked: todo.liked })}
                 style={{ cursor: "pointer" }}
               />
             ) : (
               <FaRegHeart
-                onClick={() => handleLike(todo.id, todo.liked)}
+                onClick={() => likeMutation({ id: todo.id, liked: todo.liked })}
                 style={{ cursor: "pointer" }}
               />
             )}
