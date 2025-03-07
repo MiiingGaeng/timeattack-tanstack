230937d3a039564e82fd06eee2f9565ec520157d
miing_gaeng <pem4134@naver.com>
2025-03-07 12:01:44 +0900

    feat: Home => tanstack 리팩토링

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,38 @@
-import { useState, useEffect } from "react";
 import { todoApi } from "../api/todos";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   // TODO: 필수: useQuery 로 리팩터링 하세요.
   // TODO: 선택: useQuery 로 리팩터링 후, 커스텀훅 useTodosQuery 로 정리해 보세요.
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
-
   const fetchData = async () => {
     try {
       const response = await todoApi.get("/todos");
-      setData(response.data);
+      return response.data;
     } catch (err) {
-      setError(err);
-    } finally {
-      setIsLoading(false);
+      console.err("fetch todoData error : ", err);
     }
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const {
+    data: todoData,
+    isLoading,
+    isError
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchData
+  });
 
   if (isLoading) {
     return <div style={{ fontSize: 36 }}>로딩중...</div>;
   }
 
-  if (error) {
-    console.error(error);
+  if (isError) {
+    console.error(isError);
+
     return (
-      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {error.message}</div>
+      <div style={{ fontSize: 24 }}>에러가 발생했습니다: {isError.message}</div>
     );
   }
 
@@ -40,7 +40,7 @@ export default function Home() {
     <>
       <h2>서버통신 투두리스트 by useState</h2>
       <TodoForm fetchData={fetchData} />
-      <TodoList todos={data} />
+      <TodoList todos={todoData} />
     </>
   );
 }
